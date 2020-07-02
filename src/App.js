bef09ccb8c3bde3d28d7d55bf9580534fbea8d3d
Nicolas Navarrete <nikkolasnv1997@gmail.com>
2020-07-01 23:37:46 -0400

    clean non used imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  NavLink,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // components
 import Navbar from "./components/navbar/Navbar";
