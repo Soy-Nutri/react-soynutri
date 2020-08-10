05bc34a4ccdbc57e28352af1adaa391830ab1eb0
Danilo Ibáñez <danilobepo@gmail.com>
2020-08-09 22:31:13 -0400

    create routes patients rol

---
+++ src/components/patients/Control.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const ControlsStyled = styled.div``;
+
+export default function Controls() {
+  return <ControlsStyled>Control</ControlsStyled>;
+}
